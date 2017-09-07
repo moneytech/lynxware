4c1ed528786cc87465e9e07e96c0c36cf1b2049f
Andrey Rys <rys@lynxlynx.ru>
2017-09-08 04:07:39 +0700

    strl: fix it - accept strings without newlines.

--- strl.c
+++ strl.c
@@ -2,22 +2,27 @@
 #include <unistd.h>
 #include <string.h>
 
+static char B[65536];
+
 int main(int argc, char **argv)
 {
 	int x;
-	char B[4096];
-	size_t l = 0;
+	size_t l;
 
 	if (*(argv+1)) {
-		for (x = 1; *(argv+x); x++) l += strlen(*(argv+x)); l += x-2;
+		for (l = 0, x = 1; *(argv+x); x++) l += strlen(*(argv+x));
+		l += x-2;
 		printf("%zu\n", l);
 	}
 	else {
 		while (1) {
-			fgets(B, sizeof(B), stdin);
+			l = 0;
+			fgets(B, sizeof(B)-1, stdin);
 			if (!*(B)) break;
-			l = strlen(B); l--;
+			l = strlen(B);
+			if (l && *(B+l-1) == '\n') l--;
 			printf("%zu\n", l);
+			if (l == 0) break;
 		}
 	}
 
