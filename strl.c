19431fc1e3d1391b2e6da4063821c38845172915
Andrey Rys <rys@lynxlynx.ru>
2017-09-08 11:42:58 +0700

    strl: move to getline to have to measure unlimited length strings freely.

--- strl.c
+++ strl.c
@@ -1,13 +1,15 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
-static char B[65536];
-
 int main(int argc, char **argv)
 {
+	char *ln = NULL;
 	int x;
-	size_t l;
+	size_t l, u;
 
 	if (*(argv+1)) {
 		for (l = 0, x = 1; *(argv+x); x++) l += strlen(*(argv+x));
@@ -16,15 +18,14 @@ int main(int argc, char **argv)
 	}
 	else {
 		while (1) {
-			l = 0;
-			fgets(B, sizeof(B)-1, stdin);
-			if (!*(B)) break;
-			l = strlen(B);
-			if (l && *(B+l-1) == '\n') l--;
+			l = (size_t)getline(&ln, &u, stdin);
+			if (l == (size_t)-1) break;
+			if (l && *(ln+l-1) == '\n') l--;
 			printf("%zu\n", l);
 			if (l == 0) break;
 		}
 	}
 
+	if (ln) memset(ln, 0, u);
 	return 0;
 }
